8da47b962afa0cbec29bf73420b7d19cb7ac7369
Cliffano Subagio <cliffano@gmail.com>
2026-06-04 23:18:52 +1000

    Add example configs.

--- examples/jirabatch-cli.sh
+++ examples/jirabatch-cli.sh
@@ -18,5 +18,41 @@ printf "Run command with default config file and default issues file:\n"
 jirabatch
 
 printf "\n\n========================================\n"
-printf "Run command with specified config file and issues file:\n"
-jirabatch --conf-file jirabatch.yaml --issues-file issues.yaml
+printf "Run command with specified config files creating a simple Jira epic:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file epic-simple.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating a simple Jira story:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file story-simple.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating a simple Jira task:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file task-simple.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating a simple Jira epic and a story as part of the epic:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file epic-with-story.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating a simple Jira story and a subtask as part of the story:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file story-with-subtask.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating a simple Jira task and a subtask as part of the first task:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file task-with-subtask.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating a comprehensive Jira epic:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file epic-comprehensive.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating a comprehensive Jira story:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file story-comprehensive.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating a comprehensive Jira task:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file task-comprehensive.yaml
+
+printf "\n\n========================================\n"
+printf "Run command with specified config files creating Jira epic, stories, and subtasks with default values:\n"
+jirabatch --conf-file jirabatch.yaml --issues-file defaults-epic-story-subtask.yaml
